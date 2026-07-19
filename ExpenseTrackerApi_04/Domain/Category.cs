5aa4cc543cadd048ed5d27a099fde8b5e61dc27d
jose Angel <josegallardo.cor@gmail.com>
2026-07-18 22:08:14 -0600

    feat: creacion de entidades de Category y expense

---
+++ ExpenseTrackerApi_04/Domain/Category.cs
@@ -0,0 +1,40 @@
+﻿namespace ExpenseTrackerApi_04.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        private Category()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+        public Category(string name, string description)
+        {
+            validate(name, description);
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+        }
+        public void Update(string name, string description)
+        {
+            validate(name, description);
+            Name = name;
+            Description = description;
+        }
+        private void validate(string name, string description)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Description cannot be null or empty.", nameof(description));
+            }
+        }
+    }
+}
---
+++ ExpenseTrackerApi_04/Domain/Expense.cs
@@ -0,0 +1,56 @@
+﻿namespace ExpenseTrackerApi_04.Domain
+{
+    public class Expense
+    {
+        public Guid Id { get; private set; }
+        public string Description { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTimeOffset ExpenseDate { get; private set; }
+        public Guid CategoryId { get; private set; }
+        private Expense()
+        {
+            Description = string.Empty;
+            Amount = 0;
+            ExpenseDate = DateTimeOffset.UtcNow;
+            CategoryId = Guid.Empty;
+        }
+        public Expense(string description, decimal amount, DateTimeOffset expenseDate, Guid categoryId)
+        {
+            validate(description, amount, expenseDate, categoryId);
+            Id = Guid.NewGuid();
+            Description = description;
+            Amount = amount;
+            ExpenseDate = expenseDate;
+            CategoryId = categoryId;
+        }
+        public void Update(string description, decimal amount, DateTimeOffset expenseDate, Guid categoryId)
+        {
+            validate(description, amount, expenseDate, categoryId);
+            Description = description;
+            Amount = amount;
+            ExpenseDate = expenseDate;
+            CategoryId = categoryId;
+        }
+        private void validate(string description, decimal amount, DateTimeOffset expenseDate, Guid categoryId)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Description cannot be null or empty.", nameof(description));
+            }
+
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be a positive value.", nameof(amount));
+            }
+
+            if (categoryId == Guid.Empty)
+            {
+                throw new ArgumentException("CategoryId cannot be empty.", nameof(categoryId));
+            }
+            if (expenseDate > DateTimeOffset.UtcNow)
+            {
+                throw new ArgumentException("Expense date cannot be in the future.", nameof(expenseDate));
+            }
+        }
+    }
+}
