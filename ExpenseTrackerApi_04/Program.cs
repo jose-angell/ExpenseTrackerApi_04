14e91e29b66380338462280c03c1cbf6aa657872
jose Angel <josegallardo.cor@gmail.com>
2026-07-19 23:32:56 -0600

    feat: configuracion de docker

--- ExpenseTrackerApi_04/Program.cs
+++ ExpenseTrackerApi_04/Program.cs
@@ -1,8 +1,14 @@
+using ExpenseTrackerApi_04.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
