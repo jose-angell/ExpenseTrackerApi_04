57bdf4e90d20af3a6f8ec28827f00436864a9c9b
jose Angel <josegallardo.cor@gmail.com>
2026-07-23 21:30:20 -0600

    feat: creacion de migracion de tablas

---
+++ ExpenseTrackerApi_04/Migrations/20260724032959_creaciondetablas.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApi_04.Migrations
+{
+    /// <inheritdoc />
+    public partial class creaciondetablas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cateroies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cateroies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "expenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2", nullable: false),
+                    ExpenseDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_expenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_expenses_cateroies_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "cateroies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cateroies_Name",
+                table: "cateroies",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_expenses_CategoryId",
+                table: "expenses",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "expenses");
+
+            migrationBuilder.DropTable(
+                name: "cateroies");
+        }
+    }
+}
